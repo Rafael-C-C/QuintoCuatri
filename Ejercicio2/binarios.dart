f7e86a36df87e4fd4dd8b562c23ff613794c564d
Rafael-C-C <xxrafaflooxx@gmail.com>
2022-01-29 10:23:59 -0600

    Ejercicio 2 v4

--- Ejercicio2/binarios.dart
+++ Ejercicio2/binarios.dart
@@ -1,21 +1,28 @@
 import "dart:io";
 
 void main() {
-  String opcion = "s";
-  
-  while(opcion != "n"){
-    stdout.writeln("\n Dame un decimal y lo volvere binrio");
-    int dec = int.parse(stdin.readLineSync().toString());
-    int bin = 0, i;
 
-    for (i = 1; dec > 0; i *= 10)
+  int opction = 1;
+  
+  while(opction != 2){
+
+    stdout.writeln("\n Dame un decimal y lo volvere binrio");
+    int decimal = int.parse(stdin.readLineSync().toString());
+    int binario = 0, i;
+
+    for (i = 1; decimal > 0; i *= 10)
     {
-      bin += (dec % 2) * i;
-      dec = (dec / 2).floor();
+      binario += (decimal % 2) * i;
+      decimal = (decimal / 2).floor();
     }
-    stdout.writeln("\n El numero $dec convertido a binario es: $bin");
-    stdout.writeln("\n¿Deseas convertir otro numero? (s/n)");
-    opcion = stdin.readLineSync().toString();
+    stdout.writeln("\n El numero convertido a binario es: $binario");
+
+    stdout.writeln("\n ¿Deseas convertir otro numero? (1/2)");
+
+    stdout.write("Presione 1 para aceptar y 2 para negar");
+
+    opction = int.parse(stdin.readLineSync().toString());
+
   }
 }
 
@@ -25,7 +32,7 @@ Universidad Tecnologica Metropolitana
 
    Desarrollo Móvil Multiplataforma
     Docente: Ing. Joel Ivan Chuc Uc
-    
+      
 Ejercicio 2 - practicas primer parcial
       Alumno: Cauich Colli Rafael
           Quinto cuatrimestre
