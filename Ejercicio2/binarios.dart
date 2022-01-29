19146a9a45c92f4d0e552d7d1ab942120deb23fe
Rafael-C-C <xxrafaflooxx@gmail.com>
2022-01-29 02:04:47 -0600

    Ejercicio 1 v3

--- Ejercicio2/binarios.dart
+++ Ejercicio2/binarios.dart
@@ -4,7 +4,7 @@ void main() {
   String opcion = "s";
   
   while(opcion != "n"){
-    stdout.writeln("\nDame un numero decial y lo convertire en binario");
+    stdout.writeln("\n Dame un decimal y lo volvere binrio");
     int dec = int.parse(stdin.readLineSync().toString());
     int bin = 0, i;
 
@@ -13,8 +13,23 @@ void main() {
       bin += (dec % 2) * i;
       dec = (dec / 2).floor();
     }
-    stdout.writeln("\nEl numero $dec en binario es: $bin");
-    stdout.writeln("\n¿Quieres convertir otro numero? (s/n)");
+    stdout.writeln("\n El numero $dec convertido a binario es: $bin");
+    stdout.writeln("\n¿Deseas convertir otro numero? (s/n)");
     opcion = stdin.readLineSync().toString();
   }
 }
+
+/*
+             -- U T M --
+Universidad Tecnologica Metropolitana
+
+   Desarrollo Móvil Multiplataforma
+    Docente: Ing. Joel Ivan Chuc Uc
+    
+Ejercicio 2 - practicas primer parcial
+      Alumno: Cauich Colli Rafael
+          Quinto cuatrimestre
+                Grupo A
+            Primer parcial
+
+*/
